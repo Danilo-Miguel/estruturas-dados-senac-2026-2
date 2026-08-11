77e3f380db00498675b3647b30081c795ae85408
gabilima16 <gabriela.nlima16@gmail.com>
2026-08-11 11:08:50 -0300

    refactor: Nome e números para a sona adicionados

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("Hello world!");
 
         calculateSum(5, 10, "Danilo");
+        calculateSum(30, 8, "Gabriela");
     }
 
   public static void calculateSum(int a, int b, String userName) {
