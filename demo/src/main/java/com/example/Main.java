415210540b509bc4c5ba016c46382b3c9ccd2069
leopv666 <leonardopvilla@gmail.com>
2026-08-11 11:29:56 -0300

    refactor: Nome adicionado

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("Hello world!");
 
         calculateSum(5, 10, "Danilo");
+        calculateSum(15, 6, "leonardo   ");
     }
 
   public static void calculateSum(int a, int b, String userName) {
